a316ca5e219214a8aad4bf06e8a240218cbbe454
Sandeep Mandal <2019ucp1423@mnit.ac.in>
2023-07-20 18:25:19 +0530

    added various collision checks with point

--- PhysicsEngine/include/Algo.hpp
+++ PhysicsEngine/include/Algo.hpp
@@ -6,12 +6,66 @@
 
 namespace pe {
     namespace algo {
+        static float PIE = 3.141592;
+
+        float degreeToRadian(float degree) {
+            return degree * PIE / 180;
+        }
+
+        float radianToDegree(float radian) {
+            return radian * 180 / PIE;
+        }
+
         // returns squared distance between two points
         float distance2(const pe::Vector2f& point_a, const pe::Vector2f& point_b);
 
         // returns real distance between two points
         float distance(const pe::Vector2f& point_a, const pe::Vector2f& point_b);
 
-        bool checkCircleCircleCollision(float radius1, pe::Vector2f& center1, float radius2, pe::Vector2f& center2);
+        /// <summary>
+        /// Rotates the point
+        /// </summary>
+        /// <param name="">Coordinates of the point to be rotated</param>
+        /// <param name="">Angle of rotation in anti-clockwise</param>
+        /// <param name="">(Optional argument: defaults to 0,0) Point about which rotate</param>
+        /// <returns>Rotated point</returns>
+        pe::Vector2f PointRotator(pe::Vector2f& , float , pe::Vector2f );
+
+        /// <summary>
+        /// Checks circle to circle collision
+        /// </summary>
+        /// <param name="">radius of first circle</param>
+        /// <param name="">center of first circle</param>
+        /// <param name="">radius of second circle</param>
+        /// <param name="">center of second circle</param>
+        /// <returns>true if circles do collide; else false</returns>
+        bool checkCircleCircleCollision(float , pe::Vector2f& , float , pe::Vector2f& );
+
+        /// <summary>
+        /// Checks whether point in on the line
+        /// </summary>
+        /// <param name="">Coordinates of point</param>
+        /// <param name="">Start coordinates of line</param>
+        /// <param name="">End coordinates of line</param>
+        /// <returns>True if point is on the line</returns>
+        bool checkPointLineCollision(pe::Vector2f&, pe::Vector2f&, pe::Vector2f&);
+
+        /// <summary>
+        /// Checks whether point is in the circle (including on the circle)
+        /// </summary>
+        /// <param name="">Coordinates of point</param>
+        /// <param name="">Radius of circle</param>
+        /// <param name="">Center of circle</param>
+        /// <returns>True if point is in the circle</returns>
+        bool checkPointCircleCollision(pe::Vector2f&, float&, pe::Vector2f&);
+
+        /// <summary>
+        /// Checks whether point is in the AABB box
+        /// </summary>
+        /// <param name="">Coordinates of point</param>
+        /// <param name="">Top-left(min) coordinate of AABB</param>
+        /// <param name="">Bottom-right(max) coordinate of AABB</param>
+        /// <returns>True if point is in the AABB</returns>
+        bool checkPointAABBCollision(pe::Vector2f&, pe::Vector2f&, pe::Vector2f&);
     }
 }
--- PhysicsEngine/src/Algo.cpp
+++ PhysicsEngine/src/Algo.cpp
@@ -1,19 +1,50 @@
 #include "Algo.hpp"
 
+#include <cmath>
+
 namespace pe {
     namespace algo {
-        // returns squared distance between two points
+
         float distance2(const pe::Vector2f& point_a, const pe::Vector2f& point_b) {
-            return (float)std::pow(std::abs(point_a._x - point_b._x), 2) + (float)std::pow(std::abs(point_a._y - point_b._y), 2);
+            return (point_a - point_b).getSquare();
         }
 
-        // returns real distance between two points
         float distance(const pe::Vector2f& point_a, const pe::Vector2f& point_b) {
             return std::sqrt(distance2(point_a, point_b));
+        }
+
+        pe::Vector2f PointRotator(pe::Vector2f& point, float angleDegree, pe::Vector2f center = pe::Vector2f()) {
+            pe::Vector2f shiftedPoint = point - center;
+
+            float angleRadian = degreeToRadian(angleDegree);
+            float cos = std::cos(angleRadian);
+            float sin = std::sin(angleRadian);
+
+            float rotatedX = shiftedPoint._x * cos - shiftedPoint._y * sin;
+            float rotatedY = shiftedPoint._x * sin + shiftedPoint._y * cos;
+
+            return pe::Vector2f(rotatedX, rotatedY) + center;
         }
 
         bool checkCircleCircleCollision(float radius1, pe::Vector2f& center1, float radius2, pe::Vector2f& center2) {
             return std::pow(radius1 + radius2, 2) >= distance2(center1 + radius1, center2 + radius2);
         }
+
+        bool checkPointLineCollision(pe::Vector2f& point, pe::Vector2f& lineStart, pe::Vector2f& lineEnd) {
+            float slope = (lineEnd._y - lineStart._y) / (lineEnd._x - lineStart._x);
+            float yIntercept = (lineStart._y * lineEnd._x) - (lineEnd._y * lineStart._x);
+
+            return point._y == slope * point._x + yIntercept;
+        }
+
+        bool checkPointCircleCollision(pe::Vector2f& point, float& radius, pe::Vector2f& center) {
+            return distance2(point, center) == radius * radius;
+        } 
+
+        bool checkPointAABBCollision(pe::Vector2f& point, pe::Vector2f& min, pe::Vector2f& max) {
+            return min._x <= point._x and point._x <= max._x and 
+                   min._y <= point._y and point._y <= max._y;
+        }
+
     }
 }
